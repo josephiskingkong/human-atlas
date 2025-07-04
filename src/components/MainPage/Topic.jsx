fb13d8bc88ed5878eb603b04c6405f8dae44814a
agartanah <vyatin2017@mail.ru>
2025-07-04 14:42:22 +0700

    fix: buttons of topic now work

--- src/components/MainPage/Topic.jsx
+++ src/components/MainPage/Topic.jsx
@@ -1,11 +1,13 @@
 export default function Topic({ icon, title, description }) {
   return (
-    <div className="topic">
-      <div>
-        <p className="topic-icon">{icon}</p>
-        <p className="topic-title">{title}</p>
+    <a href="human-atlas/library">
+      <div className="topic">
+        <div>
+          <p className="topic-icon">{icon}</p>
+          <p className="topic-title">{title}</p>
+        </div>
+        <p className="topic-description">{description}</p>
       </div>
-      <p className="topic-description">{description}</p>
-    </div>
+    </a>
   );
 }
