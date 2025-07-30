c4b3feffec17a990d90ce2e2f72d2e9702793606
agartanah <vyatin2017@mail.ru>
2025-07-31 04:17:09 +0700

    fix: type response for categories

--- src/hooks/categories.js
+++ src/hooks/categories.js
@@ -1,65 +1,72 @@
 import { apiRequest } from "../config/apiRequest";
 async function getMainCategories() {
-    try {
-        const response = await apiRequest(`/v1/categories/get-mains/`);
+  try {
+    const response = await apiRequest(`/v1/categories/get-mains/`);
 
-        response.sort((a, b) => a.name.localeCompare(b.name));
+    response.categories.sort((a, b) => a.name.localeCompare(b.name));
 
-        return response;
-    } catch (error) {
-        console.error('Error fetching categories: ', error.message);
-        return [];
-    }
+    return response.categories;
+  } catch (error) {
+    console.error("Error fetching categories:", error.message);
+    return [];
+  }
 }
 
 async function getCategoriesByParentId(categoryid) {
-    try {
-        const response = await apiRequest(`/v1/categories/get-by-categoryid/${categoryid}`);
+  try {
+    const response = await apiRequest(
+      `/v1/categories/get-by-categoryid/${categoryid}`
+    );
 
-        response.sort((a, b) => a.name.localeCompare(b.name));
+    response.sort((a, b) => a.name.localeCompare(b.name));
 
-        return response;
-    } catch (error) {
-        console.error('Error fetching categories: ', error.message);
-        return [];
-    }
+    return response;
+  } catch (error) {
+    console.error("Error fetching categories: ", error.message);
+    return [];
+  }
 }
 
 async function getCategoryById(id) {
-    try {
-        const response = await apiRequest(`/v1/categories/get/${id}`);
+  try {
+    const response = await apiRequest(`/v1/categories/get/${id}`);
 
-        return response;
-    } catch (error) {
-        console.error('Error fetching categories: ', error.message);
-        return [];
-    }
+    return response;
+  } catch (error) {
+    console.error("Error fetching categories: ", error.message);
+    return [];
+  }
 }
 
 async function addCategory(name, categoryid = null) {
-    const body = {
-        name,
-        categoryid
-    };
+  const body = {
+    name,
+    categoryid,
+  };
 
-    const filteredBody = Object.fromEntries(
-        Object.entries(body).filter(([_, value]) => value !== null)
-    );
+  const filteredBody = Object.fromEntries(
+    Object.entries(body).filter(([_, value]) => value !== null)
+  );
 
-    try {
-        const response = await apiRequest(`/v1/categories/add`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(filteredBody)
-        });
+  try {
+    const response = await apiRequest(`/v1/categories/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(filteredBody),
+    });
 
-        return response;
-    } catch (error) {
-        console.error('Error adding category:', error.message);
-        return null;
-    }
+    return response;
+  } catch (error) {
+    console.error("Error adding category:", error.message);
+    return null;
+  }
 }
 
-export { getCategoriesByParentId, getMainCategories, addCategory, getCategoryById };
+export {
+  getCategoriesByParentId,
+  getMainCategories,
+  addCategory,
+  getCategoryById,
+};
